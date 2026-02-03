155e61d689f531304de34dd87a6bde3562314e00
Josh Suereth <joshuasuereth@google.com>
2026-02-03 16:24:29 -0500

    Initial template package support.

--- buildscripts/test_weaver_templates.sh
+++ buildscripts/test_weaver_templates.sh
@@ -27,8 +27,8 @@ log_warn() {
 
 # Tests the output of a test agianst expected value.
 # Args:
-#     1 - Observed Output file
-#     2 - Expected Output file
+#     1 - Observed Output directory
+#     2 - Expected Output directory
 #     3 - Test name
 check_output() {
   OBSERVED_FILE="$1"
@@ -46,64 +46,75 @@ check_output() {
         # TODO - We should try to accumulate errors and report status ONCE after all tests.
         exit 1
     fi
+  elif [[ -d "$EXPECTED_FILE" ]]; then
+    # diff the directories
+    diff -r "$OBSERVED_FILE" "$EXPECTED_FILE" > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "  ✅ PASS: $TEST_NAME matches expected output."
+    else
+        echo "  ❌ FAIL: $TEST_NAME differences found!"
+        # Optional: Show the differences
+        diff -r -u "$EXPECTED_FILE" "$OBSERVED_FILE"
+        # TODO - We should try to accumulate errors and report status ONCE after all tests.
+        exit 1
+    fi
   else
-    echo "  ⚠️  SKIPPED: Missing expected file: $EXPECTED_FILE"
+    echo "  ⚠️  SKIPPED: Missing expected file or directory: $EXPECTED_FILE"
   fi
 }
 
-# Runs a policy test
-run_policy_test() {
+# Runs a template test
+run_template_test() {
   TEST_DIR="$1"
-  POLICY_PACKAGE_DIR="$2"
-  TEST_NAME=$(realpath --relative-to="$POLICY_PACKAGE_DIR" "$TEST_DIR")
-  DIAGNOSTIC_WORKAROUND=$(realpath "${POLICY_PACKAGE_DIR}/../../../diagnostic_templates")
+  TEMPLATE_PACKAGE_DIR="$2"
+  TEST_NAME=$(realpath --relative-to="$TEMPLATE_PACKAGE_DIR" "$TEST_DIR")
+  TEMPLATES_ROOT_DIR=$(realpath "${TEMPLATE_PACKAGE_DIR}/../..")
+  TEMPLATE_NAME=$(realpath --relative-to="$TEMPLATES_ROOT_DIR" "$TEMPLATE_PACKAGE_DIR")
   echo "-> Running test [${TEST_NAME}] ..."
-  OBSERVED_DIR="${POLICY_PACKAGE_DIR}/observed-output/${TEST_NAME}"
+  OBSERVED_DIR="${TEMPLATE_PACKAGE_DIR}/observed-output/${TEST_NAME}"
   rm -rf "${OBSERVED_DIR}"
   mkdir -p "${OBSERVED_DIR}"
   # Note: We force ourselves into test dir, so provenance of files is always consistently relative.
   pushd "${TEST_DIR}"
-  NO_COLOR=1 ${WEAVER} registry check \
-    -r current \
-    --baseline-registry base \
-    -p "${POLICY_PACKAGE_DIR}" \
+  NO_COLOR=1 ${WEAVER} registry generate \
+    -r registry \
     --v2 \
     --quiet \
-    --diagnostic-template="${DIAGNOSTIC_WORKAROUND}" \
-    --diagnostic-format json \
-    --diagnostic-stdout \
-    > "${OBSERVED_DIR}/diagnostic-output.json"
-    2> "${OBSERVED_DIR}/stderr"
+    --templates="${TEMPLATES_ROOT_DIR}" \
+    ${TEMPLATE_NAME} \
+    ${OBSERVED_DIR}
   popd
-  if [ $? -ne 0 ]; then
-    cat "${OBSERVED_DIR}/stderr"
-  fi
-  check_output "${OBSERVED_DIR}/diagnostic-output.json" "${TEST_DIR}/expected-diagnostic-output.json" "${TEST_NAME} - Diagnostic Output"
+  # TODO - put errors / diagnostics into a file.
+#   if [ $? -ne 0 ]; then
+#     cat "${OBSERVED_DIR}/stderr"
+#   fi
+  check_output "${OBSERVED_DIR}" "${TEST_DIR}/expected" "${TEST_NAME} - Template Output"
 }
 
 # Runs a set of policy tests for a given package.
 # This is given the test directory
 run_tests() {
-  POLICY_PACKAGE_DIR="$1"
+  TEMPLATE_PACKAGE_DIR="$1"
   TEST_DIR="$1/tests"
   if [ ! -d "$TEST_DIR" ]; then
-      log_err "Error: Tests not found in '$TEST_DIR' for policy package: ${POLICY_PACKAGE_DIR}"
+      log_err "Error: Tests not found in '$TEST_DIR' for policy package: ${TEMPLATE_PACKAGE_DIR}"
   fi
   for dir in ${TEST_DIR}/*; do
     if [ -d "${dir}" ]; then
-      run_policy_test "${dir}" "${POLICY_PACKAGE_DIR}"
+      run_template_test "${dir}" "${TEMPLATE_PACKAGE_DIR}"
     fi
   done
 }
 
 # Run all the policy package tests in the root repository.
-run_all_policy_package_tests() {
+run_all_policy_template_tests() {
   CUR="${1}"
-  if [ -d "policies" ]; then
-    for package in ${CUR}/policies/check/*; do
+  if [ -d "templates" ]; then
+    # TODO - iterate over `templates/*/*`
+    for package in ${CUR}/templates/docs/*; do
       if [ -d "${package}" ]; then
         PACKAGE_NAME=$(realpath --relative-to="$package/../.." "$package")
-        echo "---==== Policy Package - ${PACKAGE_NAME} ====---"
+        echo "---==== Template Package - ${PACKAGE_NAME} ====---"
         if [ ! -f "${package}/README.md" ]; then
           log_warn "Missing README"
         fi
@@ -122,5 +133,5 @@ if [ -d "tests" ]; then
   echo "Running tests for ${PWD}..."
   run_tests "${PWD}"
 else
-  run_all_policy_package_tests "${PWD}"
+  run_all_policy_template_tests "${PWD}"
 fi
--- policies/check/stability/README.md
+++ policies/check/stability/README.md
@@ -1,3 +1,6 @@
 # Stability Policy Package
 
 Enforces component lifecycle rules for OpenTelemetry Projects.
+
+Stability: Development
+Owners: semconv-maintainers@
